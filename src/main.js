c0916fe525f566b6b2f1323ff0db3c3e6da39f4f
Carol Gardiner <student@Makerss-MacBook-Pro.local>
2019-04-15 15:14:50 +0100

    created the html that will be created when the add to cart button is clicked

--- src/main.js
+++ src/main.js
@@ -14,4 +14,16 @@ addToCartButtonDOM.forEach((addToCartButtonDOM) => {
   
     };
 
-    
+    const inCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0);
+      
+    if(inCart === false) {
+      cartDOM.insertAdjacentHTML('beforeend', `
+        <div class="cart__item">
+          <h3 class="cart__item__name">${product.name}</h3>
+          <h3 class="cart__item__price">${product.price}</h3>
+          <h3 class="cart__item__quantity">${product.quantity}</h3>
+          <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
+          <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+          <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
+        </div>
+      `);
